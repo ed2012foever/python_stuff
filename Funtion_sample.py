fab29889d3bde27e45c847dec2e3760ae778f54b
Ed Gohery <edgohery@yahoo.co.uk>
2014-06-25 17:09:32 +0100

    Adding Function_sample file

---
+++ Funtion_sample.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+#print('Hello World')
+
+def times(x, y):
+	return x * y
+
+result = times('A', 5)
+print(result)
+
+def intersect(seq1, seq2):
+	res = []
+	for x in seq1:
+		if x in seq2:
+			res.append(x)
+	return res
+
+s1 = 'SPAM'
+s2 = 'SCAM'
+
+result = intersect(s1, s2)
+print(result)
+
